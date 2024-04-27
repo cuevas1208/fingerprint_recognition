909ea774855250d170ce79295b21251f3d6d5ee0
Doron Chapnitsky <doron.chapnitsky@ibm.com>
2024-04-27 15:28:15 +0300

    amazing

--- chains.py
+++ chains.py
@@ -19,54 +19,61 @@ class NormalizationHandler(ImageProcessingHandler):
     def process(self, data):
         image, block_size = data
         normalized_img = normalize(image.copy(), float(100), float(100))
-        return self.successor.process((normalized_img, block_size)) if self.successor else normalized_img
+        images = [normalized_img, image]
+        return self.successor.process((images, block_size)) if self.successor else normalized_img
 
 
 class SegmentationHandler(ImageProcessingHandler):
     def process(self, data):
-        image, block_size = data
-        segmented_img, normim, mask = create_segmented_and_variance_images(image, block_size, 0.2)
-        return self.successor.process((segmented_img, block_size, mask, normim)) if self.successor else segmented_img
+        images, block_size = data
+        segmented_img, normim, mask = create_segmented_and_variance_images(images[0], block_size, 0.2)
+        images.insert(0, normim)
+        images.insert(0, segmented_img)
+        return self.successor.process((images, block_size, mask)) if self.successor else segmented_img
 
 
 class OrientationHandler(ImageProcessingHandler):
     def process(self, data):
-        segmented_img, block_size, mask, normin = data
-        angles = orientation.calculate_angles(segmented_img, W=block_size, smoth=False)
-        orientation_img = orientation.visualize_angles(segmented_img, mask, angles, W=block_size)
-        return self.successor.process((orientation_img, block_size, mask, normin, angles)) if self.successor else orientation_img
+        images, block_size, mask = data
+        angles = orientation.calculate_angles(images[0], W=block_size, smoth=False)
+        orientation_img = orientation.visualize_angles(images[0], mask, angles, W=block_size)
+        images.insert(0, orientation_img)
+        return self.successor.process((images, block_size, mask, angles)) if self.successor else orientation_img
 
 
 class EnhancementHandler(ImageProcessingHandler):
     def process(self, data):
-        orientation_img, block_size, mask, normin, angles = data
-        freq = ridge_freq(orientation_img, mask, angles, block_size, kernel_size=5, minWaveLength=5, maxWaveLength=15)
-        gabor_img = gabor_filter(orientation_img, angles, freq)
-        return self.successor.process((gabor_img, block_size, mask, angles)) if self.successor else gabor_img
+        images, block_size, mask, angles = data
+        freq = ridge_freq(images[2], mask, angles, block_size, kernel_size=5, minWaveLength=5, maxWaveLength=15)
+        gabor_img = gabor_filter(images[2], angles, freq)
+        images.insert(0, gabor_img)
+        return self.successor.process((images, block_size, mask)) if self.successor else gabor_img
 
 
 class SkeletonizationHandler(ImageProcessingHandler):
     def process(self, data):
-        gabor_img, block_size, mask, angles = data
-        thin_image = skeletonize(gabor_img)
-        return self.successor.process((thin_image, block_size, mask, angles)) if self.successor else thin_image
+        images, block_size, mask = data
+        thin_image = skeletonize(images[0])
+        images.insert(0, thin_image)
+        return self.successor.process((images, block_size, mask)) if self.successor else thin_image
 
 
 class MinutiaeHandler(ImageProcessingHandler):
     def process(self, data):
-        thin_image, block_size, mask, angles = data
-        minutiae_weights_image = calculate_minutiae_weights(thin_image)
-        return self.successor.process((minutiae_weights_image, block_size, mask, angles)) if self.successor else minutiae_weights_image
+        images, block_size, mask = data
+        minutiae_weights_image = calculate_minutiae_weights(images[0])
+        images.insert(0, minutiae_weights_image)
+        return self.successor.process((images, block_size, mask)) if self.successor else minutiae_weights_image
 
 
 class BestRegionHandler(ImageProcessingHandler):
     def process(self, data):
-        minutiae_weights_image, block_size, mask, angles = data
-        best_region = get_best_region(minutiae_weights_image, block_size, mask)
-        result_image = draw_ridges_count_on_region(best_region, mask, block_size)
-        return result_image
+        images, block_size, mask = data
+        best_region = get_best_region(images[1], images[0], block_size, mask)
+        result_image = draw_ridges_count_on_region(best_region, images[-1], images[1], block_size)
+        images.insert(0, result_image)
+        return images
 
-# Processing the image
 block_size = 16
 path = os.getcwd() + '/all_png_files'
 img_name = "M89_f0115_03.png"
@@ -74,7 +81,7 @@ img_path = f'{path}/{img_name}'
 image = cv2.imread(img_path, 0)
 cv2.imshow(img_name, image)
 
-# Creating the chain
+# Chains Definition
 best_region_handler = BestRegionHandler()
 minutiae_handler = MinutiaeHandler(best_region_handler)
 skeletonization_handler = SkeletonizationHandler(minutiae_handler)
@@ -83,5 +90,7 @@ orientation_handler = OrientationHandler(enhancement_handler)
 segmentation_handler = SegmentationHandler(orientation_handler)
 normalization_handler = NormalizationHandler(segmentation_handler)
 
-# Processing the image
-result_image = normalization_handler.process((image, block_size))
+# Getting images sequence
+images = normalization_handler.process((image, block_size))
+for i, img in enumerate(images):
+    cv2.imwrite(f'result_image_chain_{i}.png', img)
